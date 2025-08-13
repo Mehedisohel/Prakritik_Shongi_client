ffa9ab97f4c2272a04166da34f4616110e4119fa
Mehedisohel <sohelmehedi39@gmail.com>
2025-08-13 21:36:28 +0600

    search bar for mobile version updated

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -76,6 +76,7 @@ const Navbar = () => {
   return (
     <div className="lg:w-11/12 mx-auto mt-3">
       <div className="navbar bg-base-100">
+        {/* Left side (logo + mobile menu) */}
         <div className="navbar-start relative">
           <div className="dropdown">
             <div
@@ -102,6 +103,9 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 absolute z-10"
             >
+              <li className="mb-2">
+                <SearchBar /> {/* Search bar visible in mobile menu */}
+              </li>
               {NavbarLinks}
             </ul>
           </div>
@@ -110,12 +114,12 @@ const Navbar = () => {
           </a>
         </div>
 
-        {/* Center menu */}
+        {/* Center menu (desktop only) */}
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1 text-lg">{NavbarLinks}</ul>
         </div>
 
-        {/* Add SearchBar here */}
+        {/* Search bar (desktop only) */}
         <div className="mx-6 hidden lg:block">
           <SearchBar />
         </div>
@@ -124,9 +128,7 @@ const Navbar = () => {
         <div className="navbar-end flex items-center">
           <div className="ml-6">
             <label className="swap swap-rotate">
-              {/* this hidden checkbox controls the state */}
               <input type="checkbox" className="theme-controller" value="dark" />
-
               {/* sun icon */}
               <svg
                 className="swap-off fill-current w-10 h-10"
@@ -135,7 +137,6 @@ const Navbar = () => {
               >
                 <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
               </svg>
-
               {/* moon icon */}
               <svg
                 className="swap-on fill-current w-10 h-10"
